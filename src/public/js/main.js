be0ed34aa431c3b8f6d9f8c06bc0cfe3e2e00c5f
Carlos Sepulveda <carlos.sepulvedahe@gmail.com>
2020-09-19 19:27:22 -0300

    chat funcional con nombre de usuario vid2

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -7,6 +7,34 @@ $(function() {
     const $messageBox = $('#message');
     const $chat = $('#chat');
 
+    //obtaning Dom elements from the interface
+
+    const $nickForm = $('#nickForm'); 
+    const $nickError = $('#nickError');
+    const $nickName = $('#nickname');
+
+    const $users = $('#usernames');
+
+
+    $nickForm.submit( e =>{
+        e.preventDefault();
+        console.log($nickName.val())
+        socket.emit('new user', $nickName.val(), data => {
+            console.log(data)
+            if (data){
+                $('#nickWrap').hide();
+                $('#contentWrap').show();
+            }else{
+                $nickError.html(`
+                    <div class="alert alert-danger">
+                        That username already exist
+                    </div>
+                `)
+            }
+            $nickName.val('');
+        });
+    })
+
     //events 
     $messageForm.submit( e => {
         e.preventDefault('enviando datos');
@@ -16,7 +44,16 @@ $(function() {
     })
 
     socket.on('new message',  function(data) {
-        $chat.append(data + '<br>')
+        $chat.append('<b>'+ data.nick + ':</b> ' + data.msg + '</br>' )
     })
+
+    socket.on('usernames', data => {
+        let html = '';
+        for (let i=0; i<data.length; i++){
+            html += `<p class="ml-2"><i class="fas fa-user"></i>${data[i]}</p>`
+        }
+        $users.html(html);
+    })
+
 
 })
--- src/sockets.js
+++ src/sockets.js
@@ -1,10 +1,45 @@
+
+
+
 module.exports = function (io) {
+
+    let nicknames = [
+        
+    ];
+
     //conexion socket del SERVER
     io.on('connection', socket =>{//io tiene a todos los clientes conectados
         console.log('new user connected');
-        socket.on('send message', function(data){
-            console.log(data)
-            io.sockets.emit('new message', data);
+
+        //eventos a los cuales esta a la escucha
+        socket.on('new user', (data, cb) => {
+            console.log("------------")
+            console.log(data);
+            if (nicknames.indexOf(data) != -1){//esta
+                cb(false);
+            }else {
+                cb(true);
+                socket.nickname = data;
+                nicknames.push(socket.nickname);
+                updateNicknames();
+            }
         })
+
+        socket.on('send message', data => {
+            io.sockets.emit('new message', {
+                msg: data,
+                nick: socket.nickname
+            });
+        })
+
+        socket.on('disconnect', data =>{
+            if (!socket.nickname) return;
+            nicknames.splice(nicknames.indexOf(socket.nickname),1)
+            updateNicknames();
+        })
+
+        function updateNicknames(){
+            io.sockets.emit('usernames', nicknames);
+        }
     })
 }
